dcaf48b5c1d1408f75ef624ded5f7d3cff980c1e
Sylvia Maina <sylviamaina16@gmail.com>
2022-01-20 14:04:38 +0300

    markdown-engine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ReactMarkdown from "react-markdown";
 
 function App() {
+  const [input, setInput] = React.useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Markdown Engine</h1>
+
+      <div className="body">
+        <textarea
+          autoFocus
+          className="textarea"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter Markdown text here"
+        />
+        <ReactMarkdown children={input} className="down" />
+      </div>
     </div>
   );
 }
